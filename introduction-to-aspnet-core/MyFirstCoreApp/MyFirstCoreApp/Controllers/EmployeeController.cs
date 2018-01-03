fcca7f14bee0e8f49ac33c41627d1cdd60527572
Eric Anastas <aireq303@gmail.com>
2018-01-02 19:30:17 -0800

    Added an Employee model class and  configured the employee index view to show some mock data

--- introduction-to-aspnet-core/MyFirstCoreApp/MyFirstCoreApp/Controllers/EmployeeController.cs
+++ introduction-to-aspnet-core/MyFirstCoreApp/MyFirstCoreApp/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyFirstCoreApp.Models;
 
 namespace MyFirstCoreApp.Controllers
 {
@@ -10,7 +11,11 @@ namespace MyFirstCoreApp.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var e = new Employee();
+            e.Eid = 69;
+            e.EmpName = "John Smith";
+
+            return View(e);
         }
     }
 }
---
+++ introduction-to-aspnet-core/MyFirstCoreApp/MyFirstCoreApp/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstCoreApp.Models
+{
+    public class Employee
+    {
+        public int Eid { get; set; }
+        public string EmpName { get; set; }
+    }
+}
