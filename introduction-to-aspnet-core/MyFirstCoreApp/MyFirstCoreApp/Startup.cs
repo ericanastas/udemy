461c0aa6e5537518c1290f7d460bad5f474b5879
Eric Anastas <aireq303@gmail.com>
2018-01-02 18:39:36 -0800

    Enabled static files. HTML files can be accessed directly now

--- introduction-to-aspnet-core/MyFirstCoreApp/MyFirstCoreApp/Startup.cs
+++ introduction-to-aspnet-core/MyFirstCoreApp/MyFirstCoreApp/Startup.cs
@@ -20,6 +20,7 @@ namespace MyFirstCoreApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
             }
 
     }
