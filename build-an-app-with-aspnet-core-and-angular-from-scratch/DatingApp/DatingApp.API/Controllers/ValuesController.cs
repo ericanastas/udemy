ea57c8b7ab327ed41c6d6858a06d2c0704b98839
Eric Anastas <aireq303@gmail.com>
2018-01-09 20:27:01 -0800

    Changed GetValues() and GetValue() to Async

--- build-an-app-with-aspnet-core-and-angular-from-scratch/DatingApp/DatingApp.API/Controllers/ValuesController.cs
+++ build-an-app-with-aspnet-core-and-angular-from-scratch/DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -20,17 +19,17 @@ namespace DatingApp.API.Controllers
 
         // GET api/values
         [HttpGet]
-        public IActionResult GetValues()
+        public async Task<IActionResult> GetValues()
         {
-            var values = _context.Values.ToList();
+            var values = await _context.Values.ToListAsync();
             return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.Values.FirstOrDefault(v=> v.Id==id);
+            var value = await _context.Values.FirstOrDefaultAsync(v=> v.Id==id);
             return Ok(value);
         }
 
