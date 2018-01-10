2b065d18b9fd1030e65991d38a679e0d1f09e3f3
Eric Anastas <aireq303@gmail.com>
2018-01-10 00:10:58 -0800

    Added CORs to allow angular app to access data in API

--- build-an-app-with-aspnet-core-and-angular-from-scratch/DatingApp/DatingApp.API/Startup.cs
+++ build-an-app-with-aspnet-core-and-angular-from-scratch/DatingApp/DatingApp.API/Startup.cs
@@ -27,6 +27,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,7 +38,8 @@ namespace DatingApp.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseCors(x=>  x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            app.UseMvc(); //Should always be last, this returns the request to the client
         }
     }
 }
