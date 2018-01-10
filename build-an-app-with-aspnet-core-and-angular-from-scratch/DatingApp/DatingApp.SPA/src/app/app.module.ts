6682ef6157a013876de9b672652993fc80d523bf
Eric Anastas <aireq303@gmail.com>
2018-01-09 22:12:40 -0800

    Added HttpModule import

--- build-an-app-with-aspnet-core-and-angular-from-scratch/DatingApp/DatingApp.SPA/src/app/app.module.ts
+++ build-an-app-with-aspnet-core-and-angular-from-scratch/DatingApp/DatingApp.SPA/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
@@ -8,9 +9,10 @@ import { ValueComponent } from './value/value.component';
   declarations: [
     AppComponent,
     ValueComponent
-  ],
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
