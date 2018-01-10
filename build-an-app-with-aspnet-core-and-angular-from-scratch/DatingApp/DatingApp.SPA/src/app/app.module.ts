991c4a33a7be6fafc0e29e55cce73bfa7b2c933a
Eric Anastas <aireq303@gmail.com>
2018-01-09 22:12:26 -0800

    Added value component

--- build-an-app-with-aspnet-core-and-angular-from-scratch/DatingApp/DatingApp.SPA/src/app/app.module.ts
+++ build-an-app-with-aspnet-core-and-angular-from-scratch/DatingApp/DatingApp.SPA/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ValueComponent
   ],
   imports: [
     BrowserModule
