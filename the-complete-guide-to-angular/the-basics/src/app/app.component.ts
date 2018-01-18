d9542aec1594e504eaef295415d889b89f53e1b5
Eric Anastas <aireq303@gmail.com>
2018-01-18 01:57:30 -0800

    Switched app component to inline styles

--- the-complete-guide-to-angular/the-basics/src/app/app.component.ts
+++ the-complete-guide-to-angular/the-basics/src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`h3{
+    color:red;
+  }`]
 })
 export class AppComponent {
 }
