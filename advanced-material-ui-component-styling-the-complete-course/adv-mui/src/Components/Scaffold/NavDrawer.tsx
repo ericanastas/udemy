2f93524416b7ecfb2c844da71c10e58fcec5d3a8
Eric Anastas <eric.anastas@som.com>
2022-09-28 04:41:53 -0700

    setup router

--- advanced-material-ui-component-styling-the-complete-course/adv-mui/src/Components/Scaffold/NavDrawer.tsx
+++ advanced-material-ui-component-styling-the-complete-course/adv-mui/src/Components/Scaffold/NavDrawer.tsx
@@ -7,30 +7,46 @@ import {
   Typography,
 } from "@mui/material";
 
+import ContactForm from "../Form/ContactForm";
+
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+
 export default function NavDrawer() {
   return (
     <div>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            Advanced MUI Styling
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="temporary" open={true}>
-        <div>
-          <List>
-            {[
-              { text: "Input Form" },
-              { text: "Contact Card Grid" },
-              { text: "Data Grid" },
-            ].map((nav, index) => (
-              <ListItem key={nav.text}>{nav.text}</ListItem>
-            ))}
-          </List>
-        </div>
-      </Drawer>
-      <main></main>
+      <BrowserRouter>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              Advanced MUI Styling
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="temporary" open={true}>
+          <div>
+            <List>
+              {[
+                { text: "Input Form", route: "/form" },
+                { text: "Contact Card Grid", route: "/grid" },
+                { text: "Data Grid", route: "/datagrid" },
+              ].map((nav, index) => (
+                <ListItem>
+                  <Link to={nav.route}>{nav.text}</Link>
+                </ListItem>
+              ))}
+            </List>
+          </div>
+        </Drawer>
+        <main>
+          <Routes>
+            <Route path="/" element={<ContactForm />}></Route>
+            <Route path="/form" element={<ContactForm />}></Route>
+            <Route path="/grid" element={<div>grid</div>}></Route>
+            <Route path="/table" element={<div>table</div>}></Route>
+            <Route path="/datagrid" element={<div>datagrid</div>}></Route>
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
