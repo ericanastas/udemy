caa26a4140927deb3fc5a4d31bf332b9950296dd
Eric Anastas <eric.anastas@som.com>
2022-10-11 00:26:40 -0700

    Adding dynamic data

--- react-the-complete-guide-incl-redux/react-complete-guide/src/components/ExpenseItem.js
+++ react-the-complete-guide-incl-redux/react-complete-guide/src/components/ExpenseItem.js
@@ -2,11 +2,15 @@ import './ExpenseItem.css'
 
 function ExpenseItem() {
 
+    const expenseDate = new Date(2022, 3, 28);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 99.99;
+
     return (<div className="expense-item">
-        <div>March 28, 2022</div>
+        <div>{expenseDate.toISOString()}</div>
         <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">Amount</div>
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__price">{expenseAmount}</div>
         </div>
     </div>)
 }
