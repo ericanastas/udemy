b12f4dc04c9de6da2116e9a2cac03aaf025fb5d3
Eric Anastas <eric.anastas@som.com>
2022-10-11 00:16:36 -0700

    styling a component

--- react-the-complete-guide-incl-redux/react-complete-guide/src/components/ExpenseItem.js
+++ react-the-complete-guide-incl-redux/react-complete-guide/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
 
-    return (<div>
-        <div>Date</div>
-        <div>
-            <h2>Title</h2>
-            <div>Amount</div>
+    return (<div className="expense-item">
+        <div>March 28, 2022</div>
+        <div className="expense-item__description">
+            <h2>Car Insurance</h2>
+            <div className="expense-item__price">Amount</div>
         </div>
     </div>)
 }
