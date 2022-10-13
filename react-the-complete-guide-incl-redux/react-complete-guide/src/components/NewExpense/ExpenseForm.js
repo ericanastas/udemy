d3de926ea6bcc2cca642b55b5394db3a615bd22a
Eric Anastas <eric.anastas@som.com>
2022-10-12 19:28:10 -0700

    added onChange handers to form and getting curent values

--- react-the-complete-guide-incl-redux/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-the-complete-guide-incl-redux/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -3,20 +3,25 @@ import "./ExpenseForm.css";
 
 function ExpenseForm() {
 
+
+    function titleChangeHandler(event) { console.log(event.target.value); }
+    function amountChangeHandler(event) { console.log(event.target.value); }
+    function dateChangeHandler(event) { console.log(event.target.value); }
+
     return (<form>
 
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" />
+                <input onChange={titleChangeHandler} type="text" />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" />
+                <input onChange={amountChangeHandler} type="number" min="0.01" step="0.01" />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" />
+                <input onChange={dateChangeHandler} type="date" min="2019-01-01" max="2022-12-31" />
             </div>
             <div className="new-expense__actions">
                 <button type="submit">Add Expense</button>
