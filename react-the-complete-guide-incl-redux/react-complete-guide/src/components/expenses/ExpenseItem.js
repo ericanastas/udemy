7f7c12de68534b2442421ba4f89b4fb1355434bd
Eric Anastas <eric.anastas@som.com>
2022-10-12 18:39:08 -0700

    Added click handler using useState()

--- react-the-complete-guide-incl-redux/react-complete-guide/src/components/expenses/ExpenseItem.js
+++ react-the-complete-guide-incl-redux/react-complete-guide/src/components/expenses/ExpenseItem.js
@@ -2,14 +2,26 @@ import './ExpenseItem.css'
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card"
 
+import { useState } from 'react'
+
 function ExpenseItem(props) {
+
+    const clickHandler = () => {
+        console.log(`title:${title}`);
+        console.log(`props.title:${props.title}`);
+        setTitle("Updated Title")
+    };
+
+    const [title, setTitle] = useState(props.title)
+
     return (<Card className="expense-item">
-        <ExpenseDate title={props.title} amount={props.amount} date={props.date} />
+        <ExpenseDate amount={props.amount} date={props.date} />
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">{props.amount}</div>
         </div>
-    </Card>)
+        <button onClick={clickHandler}>Change Title</button>
+    </Card >)
 }
 
 export default ExpenseItem;
