167012a2908be8ddd011782fdb0bf7a558597757
Eric Anastas <eric.anastas@som.com>
2022-10-17 14:01:55 -0700

    Added ExpenseFilter component to filter expenses by year

--- react-the-complete-guide-incl-redux/react-complete-guide/src/components/expenses/Expenses.js
+++ react-the-complete-guide-incl-redux/react-complete-guide/src/components/expenses/Expenses.js
@@ -1,12 +1,26 @@
 import ExpenseItem from "./ExpenseItem"
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 function Expenses(props) {
 
+    const [filteredYear, setfilteredYear] = useState("2020");
+
+
+    function onFilterChangeHanlder(year) {
+        console.log(year);
+
+        setfilteredYear(year);
+    }
+
     return (
+
         <Card className="expenses">
+            <div><ExpensesFilter filtedYear={filteredYear} onChangeFilter={onFilterChangeHanlder} /></div>
             {props.expenses.map(item => { return (<ExpenseItem key={item.id} title={item.title} date={item.date} amount={item.amount} />) })}
         </Card>
+
     )
 
 }
