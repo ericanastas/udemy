a1934ea260864c918f57441ccf152487e1a7013e
Eric Anastas <eric.anastas@som.com>
2022-10-18 02:01:52 -0700

    Added conditiona inline styles to CourseInput

--- react-the-complete-guide-incl-redux/react-styling/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react-the-complete-guide-incl-redux/react-styling/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -4,26 +4,37 @@ import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    const goalInputChangeHandler = event => {
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+        if (event.target.value.trim().length > 0) setIsValid(true);
+        setEnteredValue(event.target.value);
+    };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+    const formSubmitHandler = event => {
+        event.preventDefault();
+
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
+        else {
+
+            props.onAddGoal(enteredValue);
+        }
+    };
+
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label style={{ color: !isValid ? 'red' : "black" }}>Course Goal</label>
+                <input style={{ borderColor: !isValid ? 'red' : "black", background: !isValid ? 'salmon' : "transparent" }} type="text" onChange={goalInputChangeHandler} />
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
 };
 
 export default CourseInput;
