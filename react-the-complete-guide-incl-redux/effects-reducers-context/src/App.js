6e361c370d5f74ff83bf066583fa4c6011886c05
Eric Anastas <eric.anastas@som.com>
2022-10-20 22:02:43 -0700

    Added broken infinite loop login status

--- react-the-complete-guide-incl-redux/effects-reducers-context/src/App.js
+++ react-the-complete-guide-incl-redux/effects-reducers-context/src/App.js
@@ -5,27 +5,36 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+    //Check current login status
+    const userLoginStatus = localStorage.getItem('isLoggedIn');
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+    //if local storage has status of login setIsLoggedIn true
+    //This will create an infinite loop because setIsLoggedIn will cause the component to regenerate again
+    if (userLoginStatus === '1') setIsLoggedIn(true);
 
-  return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
-  );
+
+    const loginHandler = (email, password) => {
+        //Store login status when user logs in
+        //This is ok because it is only called when user clicks button
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false);
+    };
+
+    return (
+        <React.Fragment>
+            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+            <main>
+                {!isLoggedIn && <Login onLogin={loginHandler} />}
+                {isLoggedIn && <Home onLogout={logoutHandler} />}
+            </main>
+        </React.Fragment>
+    );
 }
 
 export default App;
