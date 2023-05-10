491d876c7b66cfc5e470652b47f769702716402a
Eric Anastas <eric.anastas@som.com>
2023-05-09 22:05:35 -0700

    updated test method to AAA with calculated expect

--- javascript-unit-testing-the-practical-guide/code/basic-testing-frontend/src/math.test.js
+++ javascript-unit-testing-the-practical-guide/code/basic-testing-frontend/src/math.test.js
@@ -2,6 +2,14 @@ import { it, expect } from 'vitest'
 import { add } from './math.js'
 
 it(`should summarize all number values in an array`, () => {
-    let result = add([1, 2, 3]);
-    expect(result).toBe(6);
+
+    //Arrange
+    let numbers = [1, 2, 3];
+
+    //Act
+    let result = add(numbers);
+
+    //Assert
+    let expectedSum = numbers.reduce((prevVal, curVal) => prevVal + curVal, 0);
+    expect(result).toBe(expectedSum);
 });
