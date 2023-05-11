eee1d64210cea33199cd59b86ad97d7aa47de323
Eric Anastas <eric.anastas@som.com>
2023-05-10 21:18:15 -0700

    added failing tests

--- javascript-unit-testing-the-practical-guide/code/basic-testing-frontend/src/math.test.js
+++ javascript-unit-testing-the-practical-guide/code/basic-testing-frontend/src/math.test.js
@@ -13,3 +13,30 @@ it(`should summarize all number values in an array`, () => {
     let expectedSum = numbers.reduce((prevVal, curVal) => prevVal + curVal, 0);
     expect(result).toBe(expectedSum);
 });
+
+
+it('should yield NaN if one invalid number is provided', () => {
+    //Arrange
+    let numbers = ['invalid', 1];
+
+    //Act
+    let result = add(numbers);
+
+    //Assert
+    expect(result).toBeNaN();
+});
+
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+
+    //Arrange
+    let numbers = ["1", "2"];
+
+    //Act
+    let result = add(numbers);
+
+    //Assert
+    let expectedSum = numbers.reduce((prevVal, curVal) => +prevVal + +curVal, 0);
+    expect(result).toBe(expectedSum);
+
+})
