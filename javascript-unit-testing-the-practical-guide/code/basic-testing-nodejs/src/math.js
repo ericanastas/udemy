1878c656c2a94c92cb8e91a8664104f3a6282df0
Eric Anastas <eric.anastas@som.com>
2023-05-11 02:59:00 -0700

    fixed math.js in other projects

--- javascript-unit-testing-the-practical-guide/code/basic-testing-nodejs/src/math.js
+++ javascript-unit-testing-the-practical-guide/code/basic-testing-nodejs/src/math.js
@@ -1,10 +1,10 @@
 function add(numbers) {
-  let sum = 0;
+    let sum = 0;
 
-  for (const number of numbers) {
-    sum += number;
-  }
-  return sum;
+    for (const number of numbers) {
+        sum += +number;
+    }
+    return sum;
 }
 
 exports.add = add;
