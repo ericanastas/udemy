8e3d717e5b39b7e2d9e61a0f7f88a6d3c7869d5c
Eric Anastas <eric.anastas@som.com>
2023-05-23 14:31:46 -0700

    added other generateResultText() tests

--- javascript-unit-testing-the-practical-guide/code/basic-testing-frontend/src/output.test.js
+++ javascript-unit-testing-the-practical-guide/code/basic-testing-frontend/src/output.test.js
@@ -1,13 +1,9 @@
-import { it, expect, describe } from "vitest"
+import { describe, it, expect } from 'vitest';
 
-import { generateResultText } from "./output.js"
+import { generateResultText } from './output';
 
-
-describe("generateResultText()", () => {
-
-
-    it("should return a string, no matter which value is passed in", () => {
-
+describe('generateResultText()', () => {
+    it('should return a string, no matter which value is passed in', () => {
         const val1 = 1;
         const val2 = 'invalid';
         const val3 = false;
@@ -16,12 +12,32 @@ describe("generateResultText()", () => {
         const result2 = generateResultText(val2);
         const result3 = generateResultText(val3);
 
-
         expect(result1).toBeTypeOf('string');
         expect(result2).toBeTypeOf('string');
         expect(result3).toBeTypeOf('string');
+    });
 
+    it('should return a string that contains the calculation result if a number is provided as a result', () => {
+        const result = 5;
 
+        const resultText = generateResultText(result);
 
-    })
-})
+        expect(resultText).toContain(result.toString());
+    });
+
+    it('should return an empty string if "no-calc" is provided as a result', () => {
+        const result = 'no-calc';
+
+        const resultText = generateResultText(result);
+
+        expect(resultText).toBe('');
+    });
+
+    it('should return a string that contains "Invalid" if "invalid" is provided as a result', () => {
+        const result = 'invalid';
+
+        const resultText = generateResultText(result);
+
+        expect(resultText).toContain('Invalid');
+    });
+});
