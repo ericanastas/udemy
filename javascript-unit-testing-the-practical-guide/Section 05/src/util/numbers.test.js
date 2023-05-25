d64c6c09560a9e3d3464c103cf69ff23e3ba8727
Eric Anastas <eric.anastas@som.com>
2023-05-24 22:42:35 -0700

    Added cleanNumbers() tests

--- javascript-unit-testing-the-practical-guide/Section 05/src/util/numbers.test.js
+++ javascript-unit-testing-the-practical-guide/Section 05/src/util/numbers.test.js
@@ -30,6 +30,31 @@ describe("transformToNumber()", () => {
         expect(result).toBeNaN();
         expect(result2).toBeNaN();
     });
+});
+
+describe("cleanNumbers()", () => {
+
+    it('should return an array of number values if an array of string number values is provided', () => {
+
+        const numberValues = ['1', '2'];
+
+        let result = cleanNumbers(numberValues);
+
+        expect(result[0]).toBeTypeOf('number');
+        expect(result[1]).toBeTypeOf('number');
+
+    });
+
+    it('should throw an error if an array with at least one empty string is provided', () => {
+
+        const numberValues = ['1', ''];
+
+        expect(() => {
+            cleanNumbers(numberValues);
+        }).toThrowError();
+
+    });
+
 
 
 });
