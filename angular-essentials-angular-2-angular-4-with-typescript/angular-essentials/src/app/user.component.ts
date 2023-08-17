027edb4c42850c0251569bea045ce5428c2ad33c
Eric Anastas <eric.anastas@som.com>
2023-08-17 01:12:47 -0700

    Changed to two way binding with NgModel

--- angular-essentials-angular-2-angular-4-with-typescript/angular-essentials/src/app/user.component.ts
+++ angular-essentials-angular-2-angular-4-with-typescript/angular-essentials/src/app/user.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-user',
-  template: `<input type="text" (input)="onUserInput($event)" [value]="name" />
+  template: `<input type="text" [(ngModel)]="name" />
     <p>Hello {{ name }}</p>
     <p>I'm the user component</p>`,
 })
 export class UserComponent {
   name = 'John Smith';
-  onUserInput(event: any) {
-    this.name = event.target.value;
-  }
 }
