dff4a9d05b16ff5902ecefcabb61201e239dcdbc
Eric Anastas <eric.anastas@som.com>
2023-08-17 00:54:00 -0700

    Adds listening to user input event

--- angular-essentials-angular-2-angular-4-with-typescript/angular-essentials/src/app/user.component.ts
+++ angular-essentials-angular-2-angular-4-with-typescript/angular-essentials/src/app/user.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-user',
-  template: `<p>{{ name }}</p>`,
+  template: `<input type="text" (input)="onUserInput($event)" />
+    <p>Hello {{ name }}</p>
+    <p>I'm the user component</p>`,
 })
 export class UserComponent {
   name = 'John Smith';
+  onUserInput(event: any) {
+    this.name = event.target.value;
+  }
 }
