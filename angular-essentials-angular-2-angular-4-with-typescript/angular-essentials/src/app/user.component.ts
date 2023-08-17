3d0d3a7034295937281ad8d3637292acef495a96
Eric Anastas <eric.anastas@som.com>
2023-08-17 01:04:18 -0700

    Switch to property binding

--- angular-essentials-angular-2-angular-4-with-typescript/angular-essentials/src/app/user.component.ts
+++ angular-essentials-angular-2-angular-4-with-typescript/angular-essentials/src/app/user.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-user',
-  template: `<input
-      type="text"
-      (input)="onUserInput($event)"
-      value="{{ name }}"
-    />
+  template: `<input type="text" (input)="onUserInput($event)" [value]="name" />
     <p>Hello {{ name }}</p>
     <p>I'm the user component</p>`,
 })
