2590a8249e99935b3ea6f141654f2de00f929514
Eric Anastas <eric.anastas@som.com>
2023-08-21 21:36:01 -0700

    Initialized CreateCharacterComponent

---
+++ angular-essentials-angular-2-angular-4-with-typescript/starwars-characters/src/app/create-character/create-character.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-create-character",
+  templateUrl: "./create-character.component.html",
+  styleUrls: ["./create-character.component.css"],
+  styles: [],
+})
+export class CreateCharacterComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
