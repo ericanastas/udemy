7e82e8b8a916ab435dfde28f1e813427b054a3a6
Eric Anastas <aireq303@gmail.com>
2024-03-11 17:44:01 -0700

    Added NewPost component

--- nextjs-react-the-complete-guide/starting-project/src/components/PostsLists.jsx
+++ nextjs-react-the-complete-guide/starting-project/src/components/PostsLists.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
 
 function PostList(props) {
   return (
-    <ul className={classes.posts}>
-      <Post author="Alice" body="ReactJS is awesome" />
-      <Post author="Bob" body="I like to code" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Alice" body="ReactJS is awesome" />
+        <Post author="Bob" body="I like to code" />
+      </ul>
+    </>
   );
 }
 export default PostList;
