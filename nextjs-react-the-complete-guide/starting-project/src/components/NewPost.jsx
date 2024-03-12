4a2bdaa19cdee62f40ab446fefd8ed31ccee5cdf
Eric Anastas <aireq303@gmail.com>
2024-03-11 18:23:25 -0700

    Lifted state up to PostsList

--- nextjs-react-the-complete-guide/starting-project/src/components/NewPost.jsx
+++ nextjs-react-the-complete-guide/starting-project/src/components/NewPost.jsx
@@ -1,23 +1,15 @@
 import classes from "./NewPost.module.css";
-import { useState } from "react";
 
-function NewPost() {
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  const [enteredBody, setEnteredBody] = useState("inital value");
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
--- nextjs-react-the-complete-guide/starting-project/src/components/PostsLists.jsx
+++ nextjs-react-the-complete-guide/starting-project/src/components/PostsLists.jsx
@@ -1,13 +1,26 @@
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
+import { useState } from "react";
 
 function PostList(props) {
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+  const [enteredBody, setEnteredBody] = useState("inital body");
+  const [enteredAuthor, setEnteredAuthor] = useState("inital author");
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Alice" body="ReactJS is awesome" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Bob" body="I like to code" />
       </ul>
     </>
