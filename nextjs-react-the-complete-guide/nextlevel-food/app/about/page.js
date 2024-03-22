a61e1479834fab4d45e9934adbc5f570273c064d
Eric Anastas <aireq303@gmail.com>
2024-03-22 03:24:06 -0700

    Added links with next/link

--- nextjs-react-the-complete-guide/nextlevel-food/app/about/page.js
+++ nextjs-react-the-complete-guide/nextlevel-food/app/about/page.js
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function AboutPage() {
     return (
         <main>
             <h1>About Us</h1>
+            <Link href="/" >Home</Link>
         </main>
     );
 }
