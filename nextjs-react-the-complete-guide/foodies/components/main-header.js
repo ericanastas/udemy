ac7d80afbacf6b49c6d045cf0bdf0fa19fffe69a
Eric Anastas <aireq303@gmail.com>
2024-03-23 18:45:28 -0700

    Added header styling

--- nextjs-react-the-complete-guide/foodies/components/main-header.js
+++ nextjs-react-the-complete-guide/foodies/components/main-header.js
@@ -1,10 +1,13 @@
 import Link from "next/link"
 import logoImage from "/assets/logo.png"
+import styles from "./main-header.module.css"
 
 export default function MainHeader() {
-    return <header><Link href="/">
-        <img src={logoImage.src} alt="A plate with food" />NextLevel Food</Link>
-        <nav>
+    return <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+            <img src={logoImage.src} alt="A plate with food" />NextLevel Food
+        </Link>
+        <nav className={styles.nav}>
             <ul>
                 <li><Link href="/meals">Meals</Link></li>
                 <li><Link href="/community">Community</Link></li>
