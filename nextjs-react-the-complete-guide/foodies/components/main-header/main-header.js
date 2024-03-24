e3dc29454589bd1c4cf19fb417ec572b8452596b
Eric Anastas <aireq303@gmail.com>
2024-03-23 18:55:32 -0700

    Moved header background into seperate component

--- nextjs-react-the-complete-guide/foodies/components/main-header/main-header.js
+++ nextjs-react-the-complete-guide/foodies/components/main-header/main-header.js
@@ -1,11 +1,17 @@
 import Link from "next/link"
 import logoImage from "/assets/logo.png"
 import styles from "./main-header.module.css"
+import Image from "next/image"
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+
 
 export default function MainHeader() {
     return <header className={styles.header}>
+
+        <MainHeaderBackground />
         <Link className={styles.logo} href="/">
-            <img src={logoImage.src} alt="A plate with food" />NextLevel Food
+            <Image priority src={logoImage} alt="A plate with food" />
+            NextLevel Food
         </Link>
         <nav className={styles.nav}>
             <ul>
