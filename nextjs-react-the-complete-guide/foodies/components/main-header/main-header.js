a6fd09a28acd95d1e5d3e990e79a014813a26db8
Eric Anastas <aireq303@gmail.com>
2024-03-30 18:40:37 -0700

    Added current link highlighting to nav links

--- nextjs-react-the-complete-guide/foodies/components/main-header/main-header.js
+++ nextjs-react-the-complete-guide/foodies/components/main-header/main-header.js
@@ -1,11 +1,17 @@
+'use client';
+
 import Link from "next/link"
 import logoImage from "/assets/logo.png"
 import styles from "./main-header.module.css"
 import Image from "next/image"
 import MainHeaderBackground from "@/components/main-header/main-header-background";
 
+import { usePathname } from "next/navigation";
+
 
 export default function MainHeader() {
+
+    const path = usePathname();
     return <header className={styles.header}>
 
         <MainHeaderBackground />
@@ -15,8 +21,8 @@ export default function MainHeader() {
         </Link>
         <nav className={styles.nav}>
             <ul>
-                <li><Link href="/meals">Meals</Link></li>
-                <li><Link href="/community">Community</Link></li>
+                <li><Link href="/meals" className={path.startsWith("/meals") ? styles.active : undefined}>Meals</Link></li>
+                <li><Link href="/community" className={path.startsWith("/community") ? styles.active : undefined}>Community</Link></li>
             </ul>
         </nav>
     </header>
