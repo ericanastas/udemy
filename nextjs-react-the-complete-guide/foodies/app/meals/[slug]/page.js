e60e8ccc1e2ead0e4b1e07bfa5869f16e301627b
Eric Anastas <aireq303@gmail.com>
2024-04-01 05:24:53 -0700

    Added meal not found page

--- nextjs-react-the-complete-guide/foodies/app/meals/[slug]/page.js
+++ nextjs-react-the-complete-guide/foodies/app/meals/[slug]/page.js
@@ -1,10 +1,13 @@
 import classes from "./page.module.css"
 import Image from "next/image";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealDetailsPage({ params }) {
 
     const meal = getMeal(params.slug);
+    if (!meal) { notFound(); }
+
     meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
 
     return <>
