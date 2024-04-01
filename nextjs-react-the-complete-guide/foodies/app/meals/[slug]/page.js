5b6586f3015b1848b28df44de8e0fe0250ee176d
Eric Anastas <aireq303@gmail.com>
2024-04-01 05:18:12 -0700

    setup meals detail page

--- nextjs-react-the-complete-guide/foodies/app/meals/[slug]/page.js
+++ nextjs-react-the-complete-guide/foodies/app/meals/[slug]/page.js
@@ -1,3 +1,26 @@
-export default function MealDetailsPage(props) {
-    return (<main><h1>Meal Details</h1><p>slug: {props.params.slug}</p></main>)
+import classes from "./page.module.css"
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+
+export default function MealDetailsPage({ params }) {
+
+    const meal = getMeal(params.slug);
+    meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
+
+    return <>
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image src={meal.image} alt={meal.title} fill />
+            </div>
+            <div className={classes.headerText}>
+                <h1>{meal.title}</h1>
+                <p>by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a></p>
+            </div>
+            <p className={classes.summary}>{meal.summary}</p>
+        </header>
+        <main>
+            <p className={classes.instructions} dangerouslySetInnerHTML={{ __html: meal.instructions }}></p>
+        </main>
+
+    </>
 }
