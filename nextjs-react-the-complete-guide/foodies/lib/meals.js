11c087bd985d85f5925fd5b7367ff396cf573473
Eric Anastas <aireq303@gmail.com>
2024-04-01 02:11:20 -0700

    Added error handling

--- nextjs-react-the-complete-guide/foodies/lib/meals.js
+++ nextjs-react-the-complete-guide/foodies/lib/meals.js
@@ -3,6 +3,7 @@ import sql from 'better-sqlite3'
 const db = sql('meals.db');
 
 export async function getMeals() {
+    //throw new Error("Loading meals failed!");
     await new Promise((resolve) => setTimeout(resolve, 2000));
     return db.prepare('SELECT * FROM meals').all();
 }
