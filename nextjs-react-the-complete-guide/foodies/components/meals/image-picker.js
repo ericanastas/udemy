eb055f4e329856a7e56e2a295d0c712f559d7159
Eric Anastas <aireq303@gmail.com>
2024-04-02 00:27:43 -0700

    replaced default html file input with custom button using useRef()

--- nextjs-react-the-complete-guide/foodies/components/meals/image-picker.js
+++ nextjs-react-the-complete-guide/foodies/components/meals/image-picker.js
@@ -1,12 +1,21 @@
+"use client";
+
+import { useRef } from "react";
+
 import classes from "./image-picker.module.css"
 export default function ImagePicker({ label, name }) {
 
+    const imageInput = useRef();
 
+    function onButtonClick(event) {
+        imageInput.current.click();
+    }
 
     return <div className={classes.picker}>
         <label htmlFor={name}>{label}</label>
         <div className={classes.controls}>
-            <input type="file" accept="image/png, image/jpeg" name={name} />
+            <input ref={imageInput} className={classes.input} type="file" accept="image/png, image/jpeg" name={name} />
+            <button className={classes.button} onClick={onButtonClick} type="button">Pick an Image</button>
         </div>
     </div>
 }
