796b09fabd6e3f6105992236589826f38284cabc
Eric Anastas <aireq303@gmail.com>
2024-04-03 06:48:29 -0700

    Added server side input validation

--- nextjs-react-the-complete-guide/foodies/lib/actions.js
+++ nextjs-react-the-complete-guide/foodies/lib/actions.js
@@ -3,6 +3,10 @@
 import { saveMeal } from "./meals";
 import { redirect } from "next/navigation";
 
+function isInvalidText(text) {
+    return !text || text.trim() === '';
+}
+
 export async function shareMeal(formData) {
     const meal = {
         title: formData.get('title'),
@@ -11,6 +15,18 @@ export async function shareMeal(formData) {
         image: formData.get('image'),
         creator: formData.get('name'),
         creator_email: formData.get('email'),
+    }
+
+    if (
+        isInvalidText(meal.title) ||
+        isInvalidText(meal.summary) ||
+        isInvalidText(meal.instructions) ||
+        isInvalidText(meal.creator) ||
+        isInvalidText(meal.creator_email) ||
+        !meal.creator_email.includes('@') ||
+        !meal.image || meal.image.size === 0
+    ) {
+        throw Error("Invalid input");
     }
 
     await saveMeal(meal);
