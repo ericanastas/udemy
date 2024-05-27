04679b10b0ac6f40de285b16aeaa076ec4e3cdb9
Eric Anastas <aireq303@gmail.com>
2024-05-27 04:46:09 -0700

    added initial parallel routes

---
+++ nextjs-react-the-complete-guide/section-4/app/archive/layout.js
@@ -0,0 +1,10 @@
+export default function ArchiveLayout({ archive, latest }) {
+    return (
+        <div>
+            <h1>News Archive</h1>
+            <section id="archive-filter">{archive}</section>
+            <h1>Archive Latest</h1>
+            <section id="archive-latest">{latest}</section>
+        </div>
+    );
+}
