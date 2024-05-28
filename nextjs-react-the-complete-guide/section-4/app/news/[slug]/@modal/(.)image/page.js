6712da876d3b13fc1c351a639af4a10df784f6c2
Eric Anastas <aireq303@gmail.com>
2024-05-28 02:53:52 -0700

    Added programatic navigation  to close modal when clicking backgroun

--- nextjs-react-the-complete-guide/section-4/app/news/[slug]/@modal/(.)image/page.js
+++ nextjs-react-the-complete-guide/section-4/app/news/[slug]/@modal/(.)image/page.js
@@ -1,15 +1,17 @@
 
+"use client";
 import { DUMMY_NEWS } from "../../../../../dummy-news"
+import { useRouter } from "next/navigation";
 
 export default function InterceptedImagePage({ params }) {
 
+    const router = useRouter();
     const newsItem = DUMMY_NEWS.find(item => item.slug === params.slug);
 
     if (!newsItem) notFound();
 
-
     return <>
-        <div className="modal-backdrop" />
+        <div className="modal-backdrop" onClick={router.back} />
         <dialog className="modal" open>
             <div className="fullscreen-image">
                 <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
